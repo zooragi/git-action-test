f1f6708f00c365b06dafd790dd041072c57babb3
zooragi <eldehd9898@gmail.com>
2022-12-02 12:09:41 +0900

    initial commit

---
+++ src/main/java/com/action/gitaction/GitactionApplication.java
@@ -0,0 +1,13 @@
+package com.action.gitaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/action/gitaction/GitactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.action.gitaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
